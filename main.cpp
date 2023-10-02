16d34e89ab873a79fe62bf9b86f6f8924382d9fd
bearagones <baragones@mail.sfsu.edu>
2023-10-01 21:25:57 -0700

    incorporated another bitrev algorithm

--- main.cpp
+++ main.cpp
@@ -79,68 +79,6 @@ vector<int> transpose(vector<int> matrix) {
     return transpose;
 }
 
-// function to perform either row or column FWHT method on a matrix
-vector<int> FWHT(const string& type, vector<int> matrix, bool bitRev) {
-
-    int k;
-
-    if (type == "column") {
-        k = numRows;
-    } else {
-        k = numColumns;
-    }
-
-    if ((k & k - 1) != 0 || k == 0) {
-        cout << endl << "FWHT cannot be performed on this matrix. Please input a matrix where there are 2^m rows or columns.";
-        exit(-1);
-    }
-
-    cout << endl << "[FWHT ANSWER]" << endl;
-
-    if (bitRev) {
-        vector<int> newMatrix(numRows * numColumns);
-        int p = 0;
-
-        while (p < numRows * numColumns) {
-            for (int j = 0; j <= k; j = j + 4) {
-                for (int i = 0; i <= 1; i++) {
-                    matrix[i + j] = matrix[i + j] + matrix[i + j + 2];
-                    matrix[i + j + 2] = matrix[i + j] - 2 * matrix[i + j + 2];
-                }
-            }
-
-            for (int j = 0; j < 2; j = j + 1) {
-                for (int l = 0; l < k; l = l + 2) {
-                    int u = matrix[j + l];
-                    int v = matrix[j + l + 4];
-                    newMatrix[p] = u + v;
-                    newMatrix[p + 1] = u - v;
-                    p += 2;
-                }
-            }
-        }
-        matrix = newMatrix;
-    } else {
-        int h = 2;
-        while (h <= k) {
-            int hf = floor(h/2);
-            for (int i = 0; i < numRows * numColumns; i = i + h) {
-                for (int j = 0; j < hf; j++) {
-                    int u = matrix[i + j];
-                    int v = matrix[i + j + hf];
-                    matrix[i + j] = u + v;
-                    matrix[i + j + hf] = u - v;
-                }
-            }
-            h *= 2;
-        }
-    }
-
-    printMatrix(type, matrix);
-
-    return matrix;
-}
-
 // function to perform bit-reversal on a given matrix
 vector<int> bitReversal(vector<int> matrix) {
 
@@ -178,31 +116,118 @@ vector<int> bitReversal(vector<int> matrix) {
     return matrix;
 }
 
+// function to perform either row or column FWHT method on a matrix
+vector<int> FWHT(const string& type, vector<int> matrix, bool bitRev, bool bitRevTwice) {
+
+    int k;
+
+    if (type == "column") {
+        k = numRows;
+    } else {
+        k = numColumns;
+    }
+
+    if ((k & k - 1) != 0 || k == 0) {
+        cout << endl << "FWHT cannot be performed on this matrix. Please input a matrix where there are 2^m rows or columns.";
+        exit(-1);
+    }
+
+    cout << endl << "[FWHT ANSWER]" << endl;
+
+    if (bitRev && !bitRevTwice) {
+        vector<int> newMatrix(numRows * numColumns);
+        int p = 0;
+
+        while (p < numRows * numColumns) {
+            for (int j = 0; j <= k; j = j + 4) {
+                for (int i = 0; i <= 1; i++) {
+                    matrix[i + j] = matrix[i + j] + matrix[i + j + 2];
+                    matrix[i + j + 2] = matrix[i + j] - 2 * matrix[i + j + 2];
+                }
+            }
+            for (int j = 0; j < 2; j = j + 1) {
+                for (int l = 0; l < k; l = l + 2) {
+                    int u = matrix[j + l];
+                    int v = matrix[j + l + 4];
+                    newMatrix[p] = u + v;
+                    newMatrix[p + 1] = u - v;
+                    p += 2;
+                }
+            }
+        }
+        matrix = newMatrix;
+    } else if (bitRevTwice) {
+        int p = 2;
+        while (p < k) {
+            for (int j = 0; j <= k; j = j + 4) {
+                for (int i = 0; i <= 1; i++) {
+                    int u = matrix[i + j];
+                    int v = matrix[i + j + p];
+                    matrix[i + j] = u + v;
+                    matrix[i + j + p] = u - v;
+                }
+            }
+            p *= 2;
+            for (int l = 0; l < k; l++) {
+                int u = matrix[l];
+                int v = matrix[l + p];
+                matrix[l] = u + v;
+                matrix[l + p] = u - v;
+            }
+        }
+        matrix = bitReversal(matrix);
+    } else {
+        int h = 2;
+        while (h <= k) {
+            int hf = floor(h/2);
+            for (int i = 0; i < numRows * numColumns; i = i + h) {
+                for (int j = 0; j < hf; j++) {
+                    int u = matrix[i + j];
+                    int v = matrix[i + j + hf];
+                    matrix[i + j] = u + v;
+                    matrix[i + j + hf] = u - v;
+                }
+            }
+            h *= 2;
+        }
+    }
+
+    printMatrix(type, matrix);
+
+    return matrix;
+}
+
 // function to measure the allotted time it takes for FWHT to perform
-void timeFWHT(bool bitRev) {
+void timeFWHT(bool bitRev, bool bitRevTwice) {
 
     auto start = high_resolution_clock::now();
     vector<int> x = makeMatrix();
 
     if (numRows >= numColumns) {
-        if (bitRev) {
-            FWHT("column", bitReversal(x), bitRev);
+        if (bitRev && !bitRevTwice) {
+            FWHT("column", bitReversal(x), bitRev, bitRevTwice);
+        } else if (bitRevTwice) {
+            FWHT("column", bitReversal(x), bitRev, bitRevTwice);
         } else {
-            FWHT("column", x, bitRev);
+            FWHT("column", x, bitRev, bitRevTwice);
         }
     } else {
-        if (bitRev) {
-            FWHT("row", bitReversal(transpose(x)), bitRev);
+        if (bitRev && !bitRevTwice) {
+            FWHT("row", bitReversal(transpose(x)), bitRev, bitRevTwice);
+        } else if (bitRevTwice) {
+            FWHT("row", bitReversal(transpose(x)), bitRev, bitRevTwice);
         } else {
-            FWHT("row", transpose(x), bitRev);
+            FWHT("row", transpose(x), bitRev, bitRevTwice);
         }
     }
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
 
-    if (bitRev) {
+    if (bitRev && !bitRevTwice) {
         cout << endl << "Allotted time with bit reversal: " << duration.count() << " microseconds" << endl;
+    } else if (bitRevTwice) {
+        cout << endl << "Allotted time with bit reversal twice: " << duration.count() << " microseconds" << endl;
     } else {
         cout << endl << "Allotted time without bit reversal: " << duration.count() << " microseconds" << endl;
     }
@@ -211,8 +236,9 @@ void timeFWHT(bool bitRev) {
 // main method
 int main() {
 
-    timeFWHT(false);
-    timeFWHT(true);
+    timeFWHT(false, false);
+    timeFWHT(true, false);
+    timeFWHT(true, true);
 
     return 0;
 }
